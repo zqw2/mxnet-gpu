483dce85f2548b072f806b93e95078491325110a
zhaoqiangwei <zhaoqiangwei2002@163.com>
2020-03-27 17:38:56 +0800

    ..

--- mlp/mlp2.py
+++ mlp/mlp2.py
@@ -1,7 +1,6 @@
 import d2lzh as d2l
-from mxnet import nd
-from mxnet.gluon import loss as gloss
-
+from mxnet import gluon, init
+from mxnet.gluon import loss as gloss, nn
 
 ########################
 #  准备几个超参
@@ -13,23 +12,12 @@ train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size)
 ########################
 #  设置网络形式
 ########################
-num_inputs, num_outputs, num_hiddens = 784, 10, 256
-W1 = nd.random.normal(scale=0.01, shape=(num_inputs, num_hiddens))
-b1 = nd.zeros(num_hiddens)
-W2 = nd.random.normal(scale=0.01, shape=(num_hiddens, num_outputs))
-b2 = nd.zeros(num_outputs)
-params = [W1, b1, W2, b2]
-for param in params:
-    param.attach_grad()
+net = nn.Sequential()
+net.add(nn.Dense(256, activation='relu'),
+        nn.Dense(10))
+net.initialize(init.Normal(sigma=0.01))
 
-def relu(X):
-    return nd.maximum(X, 0)
-
-def net(X):
-    X = X.reshape((-1, num_inputs))
-    H = relu(nd.dot(X, W1) + b1)
-    return nd.dot(H, W2) + b2
-
+trainer = gluon.Trainer(net.collect_params(), 'sgd', {'learning_rate': 0.5})
 ########################
 #  设置loss函数形式
 ########################
@@ -38,4 +26,5 @@ loss = gloss.SoftmaxCrossEntropyLoss()
 ########################
 #  开始训练
 ########################
-d2l.train_ch3(net, train_iter, test_iter, loss, num_epochs, batch_size, params, lr)
+
+d2l.train_ch3(net, train_iter, test_iter, loss, num_epochs, batch_size, None, None, trainer)
